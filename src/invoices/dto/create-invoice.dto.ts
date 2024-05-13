7c399820b2af635dc3a5ccae93eefb03da4f2606
Sahalmubarok <sahalmubarok07@gmail.com>
2024-05-13 16:07:02 +0700

    CRUD invoices

---
+++ src/invoices/dto/create-invoice.dto.ts
@@ -0,0 +1,12 @@
+export class CreateInvoiceDto {
+  price: number;
+  service_charge: number;
+  status: string;
+  receiver_longitude: string;
+  receiver_latitude: string;
+  receiver_district: string;
+  receiver_phone: string;
+  receiver_address: string;
+  receiver_name: string;
+  invoice_number: string;
+}

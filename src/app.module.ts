1a09d6080001f715d341040dc1fe3ef4fcabb527
Sahalmubarok <sahalmubarok07@gmail.com>
2024-05-13 21:56:00 +0700

    CRUD payments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { InvoicesModule } from './invoices/invoices.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule, InvoicesModule],
+  imports: [UsersModule, InvoicesModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/payments/dto/create-payment.dto.ts
@@ -0,0 +1,5 @@
+export class CreatePaymentDto {
+  bank: string;
+  amount: number;
+  status: string;
+}

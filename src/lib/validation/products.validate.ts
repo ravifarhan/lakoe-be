8f760fae9ba0052fd64babecf9ea3789d13b8a43
Sutan Arief <sutanarief21@gmail.com>
2024-05-20 16:14:31 +0700

    fix dt0, controllers, service product

--- src/lib/validation/products.validate.ts
+++ src/lib/validation/products.validate.ts
@@ -3,10 +3,18 @@ import { CreateProductDto } from 'src/products/dto/create-product.dto';
 
 export const ValidateProducts = Joi.object<CreateProductDto>({
   name: Joi.string().required(),
+  url: Joi.string().required(),
   description: Joi.string().required(),
   attachments: Joi.string().required(),
+  price: Joi.number().required(),
+  minimum_order: Joi.number().required(),
+  stock: Joi.number().required(),
+  sku: Joi.string().required(),
+  weight: Joi.number().required(),
+  long: Joi.number().required(),
+  wide: Joi.number().required(),
+  height: Joi.number().required(),
   is_active: Joi.boolean().required(),
   variants: Joi.string().empty(),
-  size: Joi.number().empty(),
-  minimum_order: Joi.number().required(),
+  size: Joi.string().empty(),
 });
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,10 +1,18 @@
 export class CreateProductDto {
   'id': string;
   'name': string;
+  'url': string;
   'description': string;
   'attachments': string;
-  'is_active': boolean;
+  'price': number;
   'minimum_order': number;
+  'stock': number;
+  'sku': string;
+  'weight': number;
+  'long': number;
+  'wide': number;
+  'height': number;
+  'is_active': boolean;
   'variants': string;
   'size': number;
   // "storesId" : string
